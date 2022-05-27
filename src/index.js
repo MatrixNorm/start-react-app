2183edd27b48f2d57d05f748a4ea3d66600c02bb
Ky Ky <kyky@kyky.ky>
2022-05-28 01:02:05 +0300

    1st

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+function Root() {
+  return <p>hi!</p>
+}
+
+ReactDOM.render(<Root />, document.getElementById("matrixnorm-react-app"));
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  target: "web",
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  watch: true,
+  watchOptions: {
+	  ignored: /node_modules/
+  },
+  devtool: "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
